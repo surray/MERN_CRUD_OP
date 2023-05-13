8f40f7f2c4f5ae683293f214d91028027d5cc817
surray <surejuly@gmail.com>
2023-05-13 20:28:04 +0530

    123

--- suray_app/src/config.js
+++ suray_app/src/config.js
@@ -2,9 +2,5 @@ import axios from "axios";
 
 export const axiosInstance =axios.create({
     baseURL:"https://my-crud.onrender.com/"
-<<<<<<< HEAD
     
-=======
-   
->>>>>>> cf3fd7624b38cdac163d6f982235f183574599c0
 })
